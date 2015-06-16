b174f3833a2701cf3391ff1d760e29d18e150924
Pranav Mayuram <pranavmayuram@Pranavs-MacBook-Pro-2.local>
2015-06-16 09:33:29 -0700

    Account creator with full Bootstrap CSS/HTML and a working page to create a basic account with email and passwod

--- LoginTrial/login.js
+++ LoginTrial/login.js
@@ -2,43 +2,55 @@
 
 //modules ====================================
 var couchbase 	= require('couchbase');
-var n1ql 		= require('couchbase').N1ql;
+var N1qlQuery 	= require('couchbase').N1qlQuery;
 var express		= require('express');
 var app 		= express();
 var bodyParser	= require('body-parser');
 var methodOverride = require('method-override');
 var morgan 		= require('morgan');
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 //config =====================================
 var port 		= process.env.PORT || 8080;
 var cluster 	= new couchbase.Cluster('http://localhost:8091');	// could later add this to a config folder OR file
 var bucket		= cluster.openBucket('loginData');
+bucket.enableN1ql("http://localhost:8093");
 
 //user operations ============================
 console.log("hello couchbase");
 
-app.post('/login/create', function(req, res) {
+app.post('/addUser', function(req, res) {
 	console.log("hello");
-	var makelog=N1qlQuery.fromString("INSERT INTO loginData (KEY, VALUE) VALUES (UUID(),{email: " + req.body.email+ ", password: "+req.body.password+"}");
-	if (req.body.password != req.body.confPassword) {
-		console.log("Confirmed password did not match password");
-	}
-	else {
-		bucket.query(makelog, function(err, result) {
+	console.log(req.body);
+	
+	var primIndex=N1qlQuery.fromString("CREATE PRIMARY INDEX ON loginData");
+	bucket.query(primIndex, function(err, result) {
 		if (err) {
-			res.send(err);
+			console.log("Index already made on loginData, OK");
+		}
+		console.log(result);
+		});
+
+	var makelog=N1qlQuery.fromString("INSERT INTO loginData (KEY, VALUE) VALUES (UUID(),{\"email\": \"" + req.body.email+ "\", \"password\": \""+req.body.password+"\"})");
+	console.log(makelog);
+	bucket.query(makelog, function(err, result) {
+		if (err) {
+			console.log(err);
+			console.log("ERROR ON NODE QUERY");
 		}
 		res.json(result);
-		console.log("Login Added!");
 		});
-	}
 });
 
+app.use(express.static(__dirname + '/public'));
+//Store all HTML files in public folder.
 
 // start app =================================
 app.get('*', function(req, res) {
-	    console.log("getting to indez.html"); // load the single view file (angular will handle the page changes on the front-end)
-        res.sendfile('./public/index.html');
+	    console.log("getting to index.html"); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendfile('index.html');
     });
 // startup our app at http://localhost:8080
 app.listen(port);               
--- LoginTrial/public/js/core.js
+++ LoginTrial/public/js/core.js
@@ -1,21 +1,21 @@
 var createLogin = angular.module('createLogin', []);
-console.log('INSIDE ANGULAR');
 
-function mainController($scope. $http) {
-	console.log('INSIDE ANGULAR controller');
+createLogin.controller("mainController", function ($scope, $http) {
 	$scope.formData={};
 
-	$scope.makeLogin = function() {
-		console.log('INSIDE ANGULAR function');
-		$http.post('/login/create', $scope.formData)
+	$scope.makeLogin = function(userInfo) {
+		console.log($scope);
+		$http({method: "POST", url: "/addUser", data: userInfo})
 			.success(function(data) {
+				console.log($scope.formData);
 				$scope.formData={};
 				$scope.create=data;
 				console.log(data);
 				console.log("SUCCESS!");
 			})
 			.error(function(data) {
+				console.log('PROBLEM!');
 				console.log('Error: ' + data);
 			});
 	};
-}
+});
