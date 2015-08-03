fe2338a4aab6478aa7efe9a83b4d2352286334a8
pranavmayuram <pranav.mayuram@couchbase.com>
2015-08-03 15:44:01 -0700

    Update stats page with full view graph

--- Touchbase/public/js/touchbase.js
+++ Touchbase/public/js/touchbase.js
@@ -473,87 +473,6 @@ touchbase.controller('statisticsController', function ($scope, $http, $window) {
 			}
 			console.log($scope.activeX);
 			$scope.loading = false;
-
-			// Chart.js Data
-    $scope.data = {
-      labels: $scope.activeX,
-      datasets: [
-        {
-          label: 'My First dataset',
-          fillColor: 'rgba(220,220,220,0.2)',
-          strokeColor: 'rgba(220,220,220,1)',
-          pointColor: 'rgba(220,220,220,1)',
-          pointStrokeColor: '#fff',
-          pointHighlightFill: '#fff',
-          pointHighlightStroke: 'rgba(220,220,220,1)',
-          data: $scope.activeTotal
-        },
-        {
-          label: 'My Second dataset',
-          fillColor: 'rgba(151,187,205,0.2)',
-          strokeColor: 'rgba(151,187,205,1)',
-          pointColor: 'rgba(151,187,205,1)',
-          pointStrokeColor: '#fff',
-          pointHighlightFill: '#fff',
-          pointHighlightStroke: 'rgba(151,187,205,1)',
-          data: $scope.activeDistinct
-        }
-      ]
-    };
-
-     
-
-    // Chart.js Options
-    $scope.options =  {
-
-      // Sets the chart to be responsive
-      responsive: true,
-
-      ///Boolean - Whether grid lines are shown across the chart
-      scaleShowGridLines : true,
-
-      //String - Colour of the grid lines
-      scaleGridLineColor : "rgba(0,0,0,.05)",
-
-      //Number - Width of the grid lines
-      scaleGridLineWidth : 1,
-
-      //Boolean - Whether the line is curved between points
-      bezierCurve : true,
-
-      //Number - Tension of the bezier curve between points
-      bezierCurveTension : 0.4,
-
-      //Boolean - Whether to show a dot for each point
-      pointDot : true,
-
-      //Number - Radius of each point dot in pixels
-      pointDotRadius : 4,
-
-      //Number - Pixel width of point dot stroke
-      pointDotStrokeWidth : 1,
-
-      //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-      pointHitDetectionRadius : 20,
-
-      //Boolean - Whether to show a stroke for datasets
-      datasetStroke : true,
-
-      //Number - Pixel width of dataset stroke
-      datasetStrokeWidth : 2,
-
-      //Boolean - Whether to fill the dataset with a colour
-      datasetFill : true,
-
-      // Function - on animation progress
-      onAnimationProgress: function(){},
-
-      // Function - on animation complete
-      onAnimationComplete: function(){},
-
-      //String - A legend template
-      legendTemplate : '<ul class="tc-chart-js-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
-    };
 			})
 			.error (function(result) {
 				console.log(error);
@@ -565,18 +484,20 @@ touchbase.controller('statisticsController', function ($scope, $http, $window) {
 			$scope.activeX=$scope.graphData.xWeek;
 			$scope.activeTotal=$scope.graphData.weekTotal;
 			$scope.activeDistinct=$scope.graphData.weekDistinct;
+			$scope.type = 'Weekly';
 		}
 		else if (type === 'day') {
 			$scope.activeX= $scope.graphData.xDay;
 			$scope.activeTotal= $scope.graphData.dayTotal;
 			$scope.activeDistinct= $scope.graphData.dayDistinct;
+			$scope.type = 'Daily';
 			console.log($scope.activeX);
 		}
 		$scope.data = {
       labels: $scope.activeX,
       datasets: [
         {
-          label: 'My First dataset',
+          label: 'Total Logins',
           fillColor: 'rgba(220,220,220,0.2)',
           strokeColor: 'rgba(220,220,220,1)',
           pointColor: 'rgba(220,220,220,1)',
@@ -586,7 +507,7 @@ touchbase.controller('statisticsController', function ($scope, $http, $window) {
           data: $scope.activeTotal
         },
         {
-          label: 'My Second dataset',
+          label: 'Distinct Logins',
           fillColor: 'rgba(151,187,205,0.2)',
           strokeColor: 'rgba(151,187,205,1)',
           pointColor: 'rgba(151,187,205,1)',
